8637571e0daecdb8b073e9f8d8816021919ce73a
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-13 00:07:38 +0100

    hid unwanted values

--- src/components/seo.js
+++ src/components/seo.js
@@ -32,42 +32,42 @@ const Seo = ({ description = '', lang = 'en', meta = [], title, image }) => {
           name: `description`,
           content: metaDescription,
         },
-        {
-          name: `image`,
-          content: image,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          property: `og:image`,
-          content: image,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary_large_image`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata?.social?.twitter || ``,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
+        // {
+        //   name: `image`,
+        //   content: image,
+        // },
+        // {
+        //   property: `og:title`,
+        //   content: title,
+        // },
+        // {
+        //   property: `og:description`,
+        //   content: metaDescription,
+        // },
+        // {
+        //   property: `og:type`,
+        //   content: `website`,
+        // },
+        // {
+        //   property: `og:image`,
+        //   content: image,
+        // },
+        // {
+        //   name: `twitter:card`,
+        //   content: `summary_large_image`,
+        // },
+        // {
+        //   name: `twitter:creator`,
+        //   content: site.siteMetadata?.social?.twitter || ``,
+        // },
+        // {
+        //   name: `twitter:title`,
+        //   content: title,
+        // },
+        // {
+        //   name: `twitter:description`,
+        //   content: metaDescription,
+        // },
       ].concat(meta)}
     />
   )
