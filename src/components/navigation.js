629aa652707874121e5a6e9a7850727aca07f81f
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-13 00:08:09 +0100

    simplified nav and added style module

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,23 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import * as styles from './navigation.module.css'
+import * as styles from '../styles/navigation.module.scss'
 
 const Navigation = () => (
-  <nav role="navigation" className={styles.container} aria-label="Main">
-    <Link to="/" className={styles.logoLink}>
-      <span className={styles.logo} />
-      <span className={styles.navigationItem}>Gatsby Starter Contentful</span>
-    </Link>
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
+  <nav role="navigation" className={styles.navigation} aria-label="Main">
+    <ul>
+      <li>
         <Link to="/" activeClassName="active">
           Home
         </Link>
       </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/" activeClassName="active">
-          Blog
+      <li>
+        <Link to="/projects/" activeClassName="active">
+          Projects
         </Link>
       </li>
     </ul>
