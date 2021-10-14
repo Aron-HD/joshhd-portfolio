acfa6ab711c6c3f8492ef513ae432a9db6db06d4
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-14 23:41:50 +0100

    built out project template page
    - expanded content model
    - expanded query
    - rendered rich text data
    - input contact details component
    - improved styles, design and layout

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,9 +1,12 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
-import { graphql } from 'gatsby'
+import { jsx, Heading, Text, Button } from 'theme-ui'
+import { graphql, Link } from 'gatsby'
+import { BLOCKS, MARKS } from '@contentful/rich-text-types'
+import { renderRichText } from 'gatsby-source-contentful/rich-text'
 
 import Layout from '../components/Layout'
 import Seo from '../components/Seo'
+import ContactInfo from '../components/ContactInfo'
 
 import * as styles from '../styles/project.module.scss'
 
@@ -11,17 +14,51 @@ export const query = graphql`
   query MyQuery($slug: String!) {
     contentfulProject(slug: { eq: $slug }) {
       title
-      id
+      body {
+        raw
+      }
     }
   }
 `
-
 const Project = (props) => {
+  const bodyRichText = props.data.contentfulProject.body
+
+  const Bold = ({ children }) => <b>{children}</b>
+
+  const options = {
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => <Text as="p">{children}</Text>,
+      [BLOCKS.HEADING_1]: (node, children) => (
+        <Heading as="h1">{children}</Heading>
+      ),
+    },
+    renderMark: {
+      [MARKS.BOLD]: (text) => <Bold>{text}</Bold>,
+    },
+  }
+
   return (
     <Layout>
       <Seo title={props.data.contentfulProject.title} />
       <article className={styles.content}>
-        <h2>{props.data.contentfulProject.title}</h2>
+        <nav className={styles.header}>
+          <div className={styles.containerLeft}>
+            <ContactInfo />
+          </div>
+          <div className={styles.containerRight}>
+            <Link to="/">
+              <Button variant="secondary">Return</Button>
+            </Link>
+          </div>
+        </nav>
+        <section className={styles.title}>
+          <Heading as="h1" variant="styles.H1">
+            {props.data.contentfulProject.title}
+          </Heading>
+        </section>
+        <section className={styles.body}>
+          {bodyRichText && renderRichText(bodyRichText, options)}
+        </section>
       </article>
     </Layout>
   )
