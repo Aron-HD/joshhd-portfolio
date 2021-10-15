3a8bdd913b6d79f446dfc021611d732d6f8deda3
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-15 21:31:21 +0100

    added foreword text and styled by section
    - enclosed main content in sections
    - wrote media queries for responsive layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import { Text } from 'theme-ui'
 
 import Layout from '../components/Layout'
 import Seo from '../components/Seo'
 import Projects from '../components/Projects'
 import ThemeSwitcher from '../components/ThemeSwitcher'
 
+import * as styles from '../styles/index.module.scss'
+
 const RootIndex = () => {
   return (
     <Layout>
       <Seo title="Home" />
-      <ThemeSwitcher />
+      <section className={styles.top}>
+        <ThemeSwitcher />
+      </section>
+      <section className={styles.foreword}>
+        <Text as="p" variant="large">
+          Joshua works in visual communication his digital media skills evolved
+          through an immersion in digital culture and Graphic Design.
+          <br />
+          <br />
+          He has a keen interest in science, technology and engineering through
+          which he reflects environment and culture.
+        </Text>
+      </section>
       <Projects />
     </Layout>
   )
