a31edf92c566fa570312dcb91784a0b1b52d6d09
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-17 16:02:29 +0100

    added expanded query to render more assets

--- src/templates/project.js
+++ src/templates/project.js
@@ -3,6 +3,7 @@ import { jsx, Heading, Text } from 'theme-ui'
 import { graphql } from 'gatsby'
 import { BLOCKS, MARKS } from '@contentful/rich-text-types'
 import { renderRichText } from 'gatsby-source-contentful/rich-text'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 import LayoutAlt from '../components/LayoutAlt'
 import Seo from '../components/Seo'
@@ -16,11 +17,23 @@ export const query = graphql`
       body {
         raw
       }
+      heroImage {
+        gatsbyImageData(
+          formats: AUTO
+          layout: CONSTRAINED
+          placeholder: BLURRED
+        )
+      }
+      heroVideo
     }
   }
 `
 const Project = (props) => {
+  const projTitle = props.data.contentfulProject.title
   const bodyRichText = props.data.contentfulProject.body
+  const heroImage = props.data.contentfulProject.heroImage
+  const heroVideo = props.data.contentfulProject.heroVideo
+  const backgroundImage = props.data.contentfulProject.heroImage // change to backgroundImage
 
   const Bold = ({ children }) => <b>{children}</b>
 
@@ -38,15 +51,44 @@ const Project = (props) => {
 
   return (
     <LayoutAlt>
-      <Seo title={props.data.contentfulProject.title} />
-      <article className={styles.content}>
+      <Seo title={projTitle} />
+      <section className={styles.title}>
+        <Heading as="h1" variant="styles.H1">
+          {projTitle}
+        </Heading>
+      </section>
+      {backgroundImage && (
+        // use gatsby-background-image plugin
         <section
-        /* className={styles.title} */
-        >
-          <Heading as="h1" variant="styles.H1">
-            {props.data.contentfulProject.title}
-          </Heading>
-        </section>
+          className={styles.backgroundImage}
+          style={{ backgroundImage: `url(${backgroundImage})` }}
+        />
+      )}
+      <article className={styles.content}>
+        {heroImage && (
+          <section className={styles.heroImageSection}>
+            <GatsbyImage
+              className={styles.heroImage}
+              image={getImage(heroImage)}
+              alt={`${projTitle} hero image`}
+            />
+          </section>
+        )}
+        {heroVideo && (
+          <section className={styles.heroVideoSection}>
+            <iframe
+              className={styles.heroVideo}
+              title={`${projTitle} hero video`}
+              src={heroVideo}
+              width="800px"
+              height="450px"
+              frameborder="0"
+              webkitallowfullscreen=""
+              mozallowfullscreen=""
+              allowfullscreen=""
+            />
+          </section>
+        )}
         <section className={styles.body}>
           {bodyRichText && renderRichText(bodyRichText, options)}
         </section>
