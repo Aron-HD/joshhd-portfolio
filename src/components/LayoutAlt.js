25eb92ab7677e942256c0862ef6b19162acf6d3a
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-17 13:04:18 +0100

    created alternate layout for projects

---
+++ src/components/LayoutAlt.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { Link } from 'gatsby'
+import { Button } from 'theme-ui'
+
+import Seo from './Seo'
+import Footer from './Footer'
+import ContactInfo from './ContactInfo'
+
+import '../styles/global.scss'
+
+const StyledHeader = styled.header`
+  padding: 1rem;
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+  /* width: 100vw; */
+  display: flex;
+  .containerLeft {
+    flex: 1;
+    text-align: left;
+  }
+  .containerRight {
+    flex: 1;
+    display: flex;
+    text-align: right;
+    align-items: center;
+    justify-content: right;
+  }
+`
+
+const LayoutAlt = ({ children }) => {
+  return (
+    <>
+      <Seo />
+      <StyledHeader>
+        <div className="containerLeft">
+          <ContactInfo />
+        </div>
+        <div className="containerRight">
+          <Link to="/">
+            <Button variant="secondary">Return</Button>
+          </Link>
+        </div>
+      </StyledHeader>
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default LayoutAlt
