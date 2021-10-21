b3485549a3c3876c7fd90719a59574444e16b989
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-21 22:55:46 +0100

    browser compatibility test

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,22 +6,43 @@ import Navigation from './Navigation'
 import ThemeSwitch from './ThemeSwitcher'
 
 const StyledHeader = styled.header`
+  display: -webkit-flex;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
   display: flex;
   padding: 1rem;
   position: -webkit-sticky; /* Safari */
   position: sticky;
   top: 0;
   width: 100vw;
-  .left {
+  .left,
+  .right {
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
     display: flex;
+    -webkit-box-align: center;
+    -moz-box-align: center;
+    -ms-flex-align: center; // IE10
+    -webkit-align-items: center; // Safari / iOS
     align-items: center;
+  }
+  .left {
+    -webkit-box-pack: flex-start;
+    -moz-box-pack: flex-start;
+    -ms-flex-pack: flex-start; // IE10
+    -webkit-justify-content: flex-start; // Safari / iOS
     justify-content: flex-start;
     flex: 1;
   }
   .right {
-    display: flex;
     text-align: right;
-    align-items: center;
+    -webkit-box-pack: flex-end;
+    -moz-box-pack: flex-end;
+    -ms-flex-pack: flex-end; // IE10
+    -webkit-justify-content: flex-end; // Safari / iOS
     justify-content: flex-end;
     flex: 1;
   }
