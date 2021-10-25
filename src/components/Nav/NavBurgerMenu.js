5d33458c9a8f733064d84dfd04bf6ef6f9120a18
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-10-25 23:40:56 +0100

    animation & theme changes & fixed position

--- src/components/Nav/NavBurgerMenu.js
+++ src/components/Nav/NavBurgerMenu.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Text } from 'theme-ui'
+/** @jsx jsx */
+import { jsx, Text } from 'theme-ui'
 import { Link } from 'gatsby'
 import { bool } from 'prop-types'
 import styled from '@emotion/styled'
@@ -11,37 +11,54 @@ const StyledMenu = styled.nav`
   justify-content: flex-start;
   height: 100vh;
   text-align: right;
-  padding: 3rem 2rem;
-  position: absolute;
+  padding: 3rem 0;
+  position: fixed;
   top: 0;
   right: 0;
-  transition: transform 0.3s ease-in-out, opacity 0.2s ease-in-out;
+  transition: transform 0.2s ease-in-out;
+  transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(100%)')};
 
-  /* Spacing for menu items */
   ul {
     padding: 0;
     list-style-type: none;
     li {
-      padding: 10px 0;
-      transition-delay: 2s;
+      cursor: pointer;
+      padding: 10px 2rem 10px 1rem;
+      margin: 5px 0;
+      /* opacity: 0.7; */
+      /* transition: opacity 1s ease; */
+      text-align: right;
       a.menu {
+        display: inline-block;
         text-decoration: none;
-        color: #00ffaa;
-        opacity: 0.9;
-        transition: 200ms;
+        width: 100%;
       }
-      a.menu:hover {
-        opacity: 0.2;
+      &:hover {
+        /* opacity: 1; */
+        text-align: left;
+        transition: text-align 0.6s;
+        a.menu {
+          /* opacity: 1; */
+          margin-right: 8rem;
+          transition: margin-right 0.5s;
+        }
       }
     }
   }
-  transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(100%)')};
 `
 
 const NavMenu = ({ open }) => {
   return (
     <StyledMenu open={open}>
-      <ul>
+      <ul
+        sx={{
+          'li:hover': {
+            bg: 'primary',
+            a: { color: 'text' },
+          },
+          a: { color: 'primary' },
+        }}
+      >
         <li>
           <Link className="menu" to="/">
             <Text variant="heading">Home</Text>
@@ -59,7 +76,7 @@ const NavMenu = ({ open }) => {
         </li>
         <li>
           <Link className="menu" to="../../../static/cv.pdf">
-            <Text variant="heading">c.v</Text>
+            <Text variant="heading">cv</Text>
           </Link>
         </li>
       </ul>
